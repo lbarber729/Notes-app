c787a0c648e071f18b1fe139bdb1215ad0d88971
Lucy Barber <lbarber729@hotmail.co.uk>
2019-06-13 11:15:07 +0100

    Added note controller getHtml method that displayes list on page - not quite working yet

--- note-controller.js
+++ note-controller.js
@@ -1,1 +1,21 @@
-// var hello = document.getElementById(app);
+"use strict";
+
+(function(exports)  {
+
+function NoteController(notelist){
+  this.notelist = notelist;
+  this.note1 = new Note('Favourite drink: seltzer');
+  this.notelist.addNote(this.note1.text);
+  this.note2 = new Note('Favourite food: pizza');
+  this.notelist.addNote(this.note2.text);
+  this.notelistview = new NoteListView(notelist);
+}
+
+NoteController.prototype.getHtml = function() {
+  var list = this.notelistview.listNotes();
+  return document.getElementById("app").innerHTML = this.notelistview.listNotes();
+
+};
+
+exports.NoteController = NoteController;
+})(this);
